cf3720670f821339f560666b3640ffbfe8858367
nathantspencer <nathantspencer@gmail.com>
2017-07-07 23:53:58 -0400

    added gitignore, read bytes from test image'g

--- sleuth/src/main.rs
+++ sleuth/src/main.rs
@@ -1,3 +1,10 @@
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("test.jpg").expect("Could not open file.");
+
+    for byte in f.bytes() {
+        println!("{}", byte.unwrap());
+    }
 }
