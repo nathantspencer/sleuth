c9e282b68acf680f114d96dc104e4a0235207ffa
nathantspencer <nathantspencer@gmail.com>
2017-07-29 23:13:06 -0400

    split exif struct/impl from main

---
+++ sleuth/src/exif.rs
@@ -0,0 +1,51 @@
+#[derive(Default)]
+pub struct Exif {
+    size_in_bytes: u16
+}
+
+impl Exif {
+    pub fn extract_exif_from_bytes(&mut self, bytes: Vec<u8>) -> () {
+        let mut app1: Vec<u8> = Vec::new();
+        let mut byte_counter = 0;
+
+        let mut last_byte = 0;
+        let mut size_byte1: u32 = 0;
+        let mut size_byte2: u32 = 0;
+
+        let mut size_byte1_set: bool = false;
+        let mut size_byte2_set: bool = false;
+        let mut in_app1: bool = false;
+
+        for byte in bytes {
+            if byte == 0xE1 && last_byte == 0xFF {
+                app1.push(0xFF);
+                app1.push(0xE1);
+                in_app1 = true;
+                last_byte = 0xE1;
+                continue;
+            }
+
+            if in_app1 && !size_byte1_set {
+                size_byte1 = byte as u32;
+                size_byte1_set = true;
+                app1.push(byte);
+            } else if in_app1 && !size_byte2_set {
+                size_byte2 = byte as u32;
+                size_byte2_set = true;
+                app1.push(byte);
+                byte_counter = 2;
+            } else if in_app1 && byte_counter < size_byte1 * 255 + size_byte2 {
+                app1.push(byte);
+                byte_counter = byte_counter + 1;
+            }
+
+            last_byte = byte;
+        }
+
+        self.size_in_bytes = byte_counter as u16;
+    }
+
+    pub fn get_size_in_bytes(&self) -> u16 {
+        self.size_in_bytes
+    }
+}
--- sleuth/src/main.rs
+++ sleuth/src/main.rs
@@ -3,6 +3,9 @@ use std::fs::File;
 use std::process;
 use std::io::prelude::*;
 
+use exif::Exif;
+mod exif;
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
@@ -19,49 +22,8 @@ fn main() {
         bytes.push(byte.unwrap());
     }
 
-    let app1 = get_app1_segment(bytes);
-    let app1_size = app1.len();
-    println!("Size of APP1 Segment: {}", app1_size);
-}
+    let mut test: Exif = Default::default();
+    test.extract_exif_from_bytes(bytes);
 
-
-fn get_app1_segment(bytes: Vec<u8>) -> Vec<u8> {
-    let mut app1: Vec<u8> = Vec::new();
-    let mut byte_counter = 0;
-
-    let mut last_byte = 0;
-    let mut size_byte1: u32 = 0;
-    let mut size_byte2: u32 = 0;
-
-    let mut size_byte1_set: bool = false;
-    let mut size_byte2_set: bool = false;
-    let mut in_app1: bool = false;
-
-    for byte in bytes {
-        if byte == 0xE1 && last_byte == 0xFF {
-            app1.push(0xFF);
-            app1.push(0xE1);
-            in_app1 = true;
-            last_byte = 0xE1;
-            continue;
-        }
-
-        if in_app1 && !size_byte1_set {
-            size_byte1 = byte as u32;
-            size_byte1_set = true;
-            app1.push(byte);
-        } else if in_app1 && !size_byte2_set {
-            size_byte2 = byte as u32;
-            size_byte2_set = true;
-            app1.push(byte);
-            byte_counter = 2;
-        } else if in_app1 && byte_counter < size_byte1 * 255 + size_byte2 {
-            app1.push(byte);
-            byte_counter = byte_counter + 1;
-        }
-
-        last_byte = byte;
-    }
-
-    app1
+    println!("Size of APP1 Segment: {}", test.get_size_in_bytes());
 }
